20279141300de7205fd49fd51213bb4bcb234c4d
Simarjit <simarjassal9@gmail.com>
2022-04-01 08:17:27 -0400

    automation practice

--- FrameworkConcept/src/main/java/com/automationPractice/Pages/ShoppingCartSummaryPage.java
+++ FrameworkConcept/src/main/java/com/automationPractice/Pages/ShoppingCartSummaryPage.java
@@ -1,8 +1,15 @@
 package com.automationPractice.Pages;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import com.automationPractice.BasePackage.TestBase;
@@ -16,21 +23,24 @@ public class ShoppingCartSummaryPage extends TestBase {
 	@FindBy(css = "a[title='Delete']")
 	WebElement deleteBtn;
 
-	/*
-	 * public void totalPriceAssert() { Assert.assertEquals(totalPrice.getText(),
-	 * "$35.02"); }
-	 */
-	@FindBy(css = "p[class='alert alert-warning']")
-	WebElement deletedMessageText;
+	@FindBy(className = "alert")
+	WebElement shoppingCartEmptyAlert;
 
 	public ShoppingCartSummaryPage deleteItem() {
 		deleteBtn.click();
 
 		return new ShoppingCartSummaryPage();
+
 	}
 
-	public String successfulDeleteMessage() {
-		return deletedMessageText.getText();
+	public String getAlertMessage() {
 
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return shoppingCartEmptyAlert.getText();
 	}
 }
